5037a3319d89e504d271841efe1049c9ebef5455
jusong23 <jusong23@naver.com>
2022-11-29 23:33:57 +0900

    Complete

--- main2.cpp
+++ main2.cpp
@@ -7,21 +7,29 @@
 #include <sys/types.h>
 #include <unistd.h>
 
-int main()
-{
-    // MenuBoardList.dat 파일을 읽기
+int main() {
+    // StudentList.dat 파일을 읽기
     MenuBoard std;
-    char *pathname =  (char*)"./MenuBoardList.dat";
+    char *pathname = (char *)"./MenuBoardList.dat";
 
-    int fd = open(pathname, O_RDONLY);
+    list<MenuBoard> StudentList;
+    list<MenuBoard>::iterator it;
+
+    int fd = open(pathname, O_RDONLY); //.dat파일 오픈
     struct stat fileInfo;
     fstat(fd, &fileInfo);
 
-    for (int i = 0; i < fileInfo.st_size / sizeof(MenuBoard); i++)
-    {
+    for (int i = 0; i < fileInfo.st_size / sizeof(MenuBoard); i++) {
+        // FIFO방식으로 Menu 정보를 자료구조에 대입
         read(fd, &std, sizeof(MenuBoard));
-        cout << "Menu : " << std.getName() << " Score : " << std.getoption()
-             << " Price : " << std.getprice() << endl;
+        StudentList.push_back(std);
     }
-    printf("show %llu menu\n", fileInfo.st_size / sizeof(MenuBoard));
+
+    cout << "[Menu]\t[Score]\t[Price]" << endl;
+    for (it = StudentList.begin(); it != StudentList.end(); it++) {
+        cout << it->getName() << "\t  " << it->getoption() << "\t  "
+             << it->getprice() << endl;
+    }
+
+    printf("   ---show %ld menu---\n", StudentList.size());
 }
